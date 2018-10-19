7d9cf8d18557980aed7bc5a05602c1771b94598b
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-20 00:51:51 +0300

    Add files via upload

---
+++ Binary Search/Binary Search/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+            int başlangıç = 0, bitiş = sayilar.GetUpperBound(0), orta = (başlangıç + bitiş) / 2, arananSayi = 0;
+            Console.WriteLine("Aranacak sayıyı giriniz = ");
+            arananSayi = Convert.ToInt32(Console.ReadLine());
+            while (başlangıç <= bitiş)
+            {
+                orta = (başlangıç + bitiş) / 2;
+                if (sayilar[orta] > arananSayi)
+                {
+                    bitiş = orta - 1;
+                }
+                else if (sayilar[orta] < arananSayi)
+                {
+                    başlangıç = orta + 1;
+                }
+                else
+                {
+
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
